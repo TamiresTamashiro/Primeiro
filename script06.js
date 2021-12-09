852bde43d0b0e39e79bec83d101648edf55438e5
Tamires <94778715+TamiresTamashiro@users.noreply.github.com>
2021-12-09 09:08:37 -0300

    Update script06.js

--- script06.js
+++ script06.js
@@ -6,25 +6,19 @@ function calcular(){
     var tempo= Number(tempoDeestudo.value)
     var dia= Number(diasDeestudo.value)
     var res= document.querySelector('div#resultado')
- var conta = (tempo+dia)/2
+    var conta = (tempo+dia)/2
     
-if(conta <=10 ){
+if (conta <=10 ){
     res.innerHTML= 'Precisa se dedicar mais ...'
 
-}else if (conta >=11 && conta <=20) {
-    res.innerHTML='Voce esta bem continue assim! '
+} else if (conta >=11 && conta <=20) {
+  res.innerHTML='Voce esta bem continue assim! '
     
-} else  { 
-    res.innerHTML='Parabéns voce esta estudando bastante !! Voce vai longe !!'
+} else { 
+  res.innerHTML='Parabéns voce esta estudando bastante !! Voce vai longe !!'
     
-}
-
-
-
-   
-        
-   
-        
+}  
+    
 }
 
     
